151aa56f8e0571d8b50395b09a3b3be899b1dbfa
Sofia <151773@herzen.spb.ru>
2024-10-08 16:19:54 +0300

    Добавьте файлы проекта.

---
+++ Task1/Angle.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        //Свойство для градусов
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value;
+            }
+        }
+        //Свойство для минут
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    if (value > 60)
+                    {
+                        Gradus += value / 60;
+                        min = value % 60;
+                    }
+                    else
+                    {
+                        min = value;
+                    }
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+            }
+        }
+        //Свойство для секунд
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    if (value > 60)
+                    {
+                        Gradus += value / 60;
+                        sec = value % 60;
+                    }
+                    else
+                    {
+                        sec = value;
+                    }
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+            }
+        }
+        //конструктор
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        //метод для перевода угла в радианы
+        public double ToRadians()
+        {
+            return (Math.PI * (gradus + min / 60.0 + sec / 3600.0)) / 180;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            Console.WriteLine("Перевод угла из градусов, минут и секунд в радианы:\n\n");
+            //try..catch на случай, если пользователь, например, случайно вместо числа введет символ
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Введите градусы: ");
+                    int gradus = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите минуты: ");
+                    int min = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите секунды: ");
+                    int sec = Convert.ToInt32(Console.ReadLine());
+
+                    angle = new Angle(gradus, min, sec);
+                }
+                while (!angle.isCorrect);
+
+                double radians = angle.ToRadians();
+                Console.WriteLine("Введенный угол в радианах равен: {0}", radians);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
